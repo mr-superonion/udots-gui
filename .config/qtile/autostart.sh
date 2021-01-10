583db3f50924a2f7b97ea27adee43b7d593a7b06
Xiangchong <xiangchong.li@ipmu.jp>
2021-01-10 23:28:04 +0900

    add picom

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 
-[ "$(which picom 2>/dev/null)" ] && picom &
+[ "$(which picom 2>/dev/null)" ] && picom --experimental-backends &
 [ "$(which blueberry-tray 2>/dev/null)" ] && blueberry-tray &
 [ "$(which blueman-tray 2>/dev/null)" ] && blueman-tray &
 [ "$(which nm-applet 2>/dev/null)" ] && nm-applet &
 [ "$(which pasystray 2>/dev/null)" ] && pasystray &
+
 updateWeather
 displaySetup
